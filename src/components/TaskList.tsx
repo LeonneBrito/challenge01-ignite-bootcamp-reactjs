01aeac1b3a273945780a49ea5335012bd0a8243c
LeonneBrito <contato@leonnebrito.com.br>
2021-03-08 15:35:42 -0300

    :tada: feat: add task crud

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,33 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    setTasks([...tasks, {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }])
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {
+          id: task.id,
+          title: task.title,
+          isComplete: !task.isComplete
+        }
+      }
+      return task;
+    })
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = [...tasks]
+    newTasks.splice(id, 1)
+    setTasks(newTasks)
   }
 
   return (
